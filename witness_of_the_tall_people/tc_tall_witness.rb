256b2b8d5b3b380d50fa68d1567d05fb82480fce
Johan Baaij <johanbaaij@gmail.com>
2019-12-18 14:28:53 +1100

    refactor: test method name

--- witness_of_the_tall_people/tc_tall_witness.rb
+++ witness_of_the_tall_people/tc_tall_witness.rb
@@ -2,7 +2,7 @@ require_relative 'tall_witness'
 require 'test/unit'
 
 class TestTallWitness < Test::Unit::TestCase
-  def test_simple
+  def test_tall_witness
     assert_equal(3, TallWitness.new([3, 6, 3, 4, 1]).witnesses)
     assert_equal(2, TallWitness.new([6, 3, 3, 3, 3]).witnesses)
     assert_equal(1, TallWitness.new([6, 3, 3, 3, 8]).witnesses)
